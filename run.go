60ed60e48d360c4e283bedae016be46ce3315264
ZiomaleQ <r.partyka30@gmail.com>
2024-01-28 21:22:32 +0100

    Discord bot key setup

--- run.go
+++ run.go
@@ -1,13 +1,43 @@
 package main
 
 import (
+	"embed"
 	"fmt"
 	"os"
 	"sao/player"
 	"sao/world"
 )
 
+//go:embed botkey.txt
+var botkey embed.FS
+
 func main() {
+
+	botkeyRaw, err := botkey.Open("botkey.txt")
+
+	if err != nil {
+		fmt.Println("Cannot read config file")
+		return
+	}
+
+	botKeyFileStats, err := botkeyRaw.Stat()
+
+	if err != nil {
+		fmt.Println("Cannot read config file")
+		return
+	}
+
+	botKey := make([]byte, botKeyFileStats.Size())
+
+	_, err = botkeyRaw.Read(botKey)
+
+	if err != nil {
+		fmt.Println("Cannot read config file")
+		return
+	}
+
+	fmt.Println(string(botKey))
+
 	world := world.CreateWorld(os.Args[1] == "test")
 
 	go world.StartClock()
