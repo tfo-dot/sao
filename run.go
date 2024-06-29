efc480722dabe633677476f13218632e58a18bd3
ZiomaleQ <r.partyka30@gmail.com>
2024-06-29 22:14:49 +0200

    Remove test from run.go

--- run.go
+++ run.go
@@ -4,11 +4,8 @@ import (
 	"embed"
 	"fmt"
 	"os"
-	"sao/data"
 	"sao/discord"
 	"sao/world"
-
-	"github.com/google/uuid"
 )
 
 //go:embed botkey.txt
@@ -47,12 +44,6 @@ func main() {
 	world := world.CreateWorld(string(botKey), isTest)
 
 	world.LoadBackup()
-
-	tp := world.Players[uuid.MustParse("e2eb0ff0-f98e-47e4-b90c-86a21dc58259")]
-
-	itemTemp := data.Items[data.AttackVisageUUID]
-
-	tp.AddItem(&itemTemp)
 
 	go world.StartClock()
 
